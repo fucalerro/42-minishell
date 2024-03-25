e72b03ab640fbdbc9b469c05126d20aa6054c497
lnicolli <lnicolli@c1r9s4.42lausanne.ch>
2024-03-25 13:43:28 +0100

    some basic shell"

--- src/main.c
+++ src/main.c
@@ -1,31 +1,20 @@
 #include "minishell.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 extern char **environ;
 
 #ifndef UNIT_TESTS
 int main(int ac, char **av, char **env)
 {
-    // char *user_input = "a b |c d| e f";
-
-    char *user_input = av[1];
-
-    printf("%s\n\n", user_input);
-    // user_input = readline("minishell: ");
-    // printf("%d\n", is_in_quotes(user_input, 400));
-
-    // printf("%s\n", input_normalizer(user_input));
-    // printf("%d\n", word_counter_quotes(user_input));
-
-    tokenizer(user_input);
-
-    // get_files_list("src");
-
-    printf("%s\n", var_expander("$PATH"));
-
-
-    // parser(user_input);
-
-
+	char *line = "minishel: ";
+	char *promt;
+	while((promt = readline(line)))
+	{
+		printf("I got : %s\n", promt);
+	}
     return (0);
 }
 #endif
