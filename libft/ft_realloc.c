22b10870807298053399391e44a4e435caa1ce12
fucalerro <hello@lucaferro.ch>
2024-03-28 15:20:06 +0100

    fix ft_realloc

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: lferro <lferro@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 18:01:49 by lferro            #+#    #+#             */
-/*   Updated: 2024/02/25 18:12:19 by lferro           ###   ########.fr       */
+/*   Updated: 2024/03/28 15:19:51 by lferro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@ void    *ft_realloc(void *ptr, size_t old_size, size_t size)
 {
     void    *new_block;
 
-    if (ptr == NULL)
-        return (ptr);
-    if (old_size == size)
-        return (ptr);
-    new_block = malloc(size);
-    if (new_block == NULL)
+    if (!ptr)
+        return (malloc(size));
+        if (size == 0) {
+        free(ptr);
         return (NULL);
-    if (old_size < size)
-        ft_memcpy(new_block, ptr, old_size);
-    else
-        ft_memcpy(new_block, ptr, old_size);
+    }
+    new_block = malloc(size);
+    if (!new_block)
+        return (NULL);
+    ft_memcpy(new_block, ptr, old_size < size ? old_size : size);
     free(ptr);
     return (new_block);
 }
