f7fdb6c7713239301fdc3c357a747463fbeba20e
lnicolli <lnicolli@c1r11s8.42lausanne.ch>
2024-03-29 17:16:47 +0100

    improved pipe A LOT

--- src/exec.c
+++ src/exec.c
@@ -37,6 +37,40 @@ char	*get_cmd_path(char *raw, char **path)
 	return (NULL);
 }
 
+int check_pipe(t_node *node)
+{
+	int return_value;
+
+	return_value = 0;
+	if(node && node->next && node->next->type == T_PIPE)
+		return_value += 1;
+	if(node && node->previous && node->previous->type == T_PIPE)
+		return_value += 2;
+	return(return_value);
+}
+int set_pipe(t_node *node)
+{
+	int check_value;
+	int  (*pipe_fd)[2];
+
+	check_value = check_pipe(node);
+	if (check_value & PIPE_NEXT)
+	{
+		pipe_fd = node->next->pipe;
+		close(pipe_fd[0][0]);
+		dup2(pipe_fd[0][1], STDOUT_FILENO);
+		close(pipe_fd[0][1]);
+	}
+	if (check_value & PIPE_PREVIOUS)
+	{
+		pipe_fd = node->previous->pipe;
+		close(pipe_fd[0][1]);
+		dup2(pipe_fd[0][0], STDIN_FILENO);
+		close(pipe_fd[0][0]);
+	}
+	return 0;
+}
+
 char	**get_path(char *env[])
 {
 	char	**tmps;
@@ -82,62 +116,26 @@ int is_builtin(char **cmd, t_hist **hist, char ***env)
 int run_cmd(char **path, t_node *node, t_hist **hist, t_stack **pid_stack, char ***env)
 {
 	pid_t	pid;
-	int  (*pipe_fd)[2];
 	char **cmd;
 
 	if (node->next && node->next->type == T_PIPE)
-	{
 		pipe(node->next->pipe[0]);
-	}
+
 	if (is_builtin(node->cmd, hist, env))
 		debug_print("this node is CMD builtin");
 	else
 	{
-		if (node->next && node->next->type == T_PIPE) //first child/parent
+		node->cmd[0] = get_cmd_path(node->cmd[0], path);
+		cmd = node->cmd;
+		pid = fork();
+		if (pid == 0)
 		{
-			debug_print("this node is CMD with next PIPE");
-			node->cmd[0] = get_cmd_path(node->cmd[0], path);
-			cmd = node->cmd;
-			pipe_fd = node->next->pipe;
-			pid = fork();
-			if (pid == 0)
-			{
-				close(pipe_fd[0][0]);
-				dup2(pipe_fd[0][1], STDOUT_FILENO);
-				close(pipe_fd[0][1]);
-				execve(cmd[0], cmd, NULL);
-				printf("fail1\n");
-			    exit(EXIT_FAILURE);
-			}
-			else
-				stack_add(pid_stack, pid);
-		}
-		else if (node->previous && node->previous->type == T_PIPE) //second child/parent
-		{
-			debug_print("this node is CMD with previous PIPE");
-			node->cmd[0] = get_cmd_path(node->cmd[0], path);
-			cmd = node->cmd;
-			pipe_fd = node->previous->pipe;
-			pid = fork();
-			if (pid == 0)
-			{
-				close(pipe_fd[0][1]);
-				dup2(pipe_fd[0][0], STDIN_FILENO);
-				close(pipe_fd[0][0]);
-				execve(cmd[0], cmd, NULL);
-				printf("fail2 with %s and %s\n",cmd[0], cmd[1]);
-			    exit(EXIT_FAILURE);
-			}
-			else
-				stack_add(pid_stack, pid);
+			set_pipe(node);
+			execve(cmd[0], cmd, NULL);
+			exit(EXIT_FAILURE);
 		}
 		else
-		{
-			debug_print("this node is CMD");
-			node->cmd[0] = get_cmd_path(node->cmd[0], path);
-			if(node->cmd)
-				exec(node->cmd, pid_stack);
-		}
+			stack_add(pid_stack, pid);
 	}
 }
 
