3ab8cb018b0950409781909f9e79da645dbc2d05
Lucas Nicollier <lucas.nicollier@gmail.com>
2024-04-04 23:14:14 +0200

    creating tools for list manipulation

--- src/list_utils.c
+++ src/list_utils.c
@@ -1,5 +1,31 @@
 #include "minishell.h"
 
+int switch_node_n_node_next(t_node **lst, t_node *node)
+{
+	t_node *node_prev;
+	t_node *node_next;
+	t_node *node_next2;
+
+	if (node->next)
+	{
+		node_next = node->next;
+		node_next2 = node->next->next;
+		node_prev = node->previous;
+
+		node->next = node_next2;
+		node->previous = node_next;
+		if(node_next2)
+			node_next2->previous = node;
+		node_next->next = node;
+		node_next->previous = node_prev;
+		if (node_prev)
+			node_prev->next = node_next ;
+		else
+			*lst = node_next;
+	}
+	return (0);
+}
+
 int sort_infile(t_node **lst)
 {
 	t_node *node;
@@ -12,23 +38,25 @@ int sort_infile(t_node **lst)
 	while(node)
 	{
 		if (node->next && node->type == T_CMD && node->next->type == T_INFILE)
-		{
-			node_next = node->next;
-			node_next2 = node->next->next;
-			node_prev = node->previous;
-
-			node->next = node_next2;
-			node->previous = node_next;
-			if(node_next2)
-				node_next2->previous = node;
-			node_next->next = node;
-			node_next->previous = node_prev;
-			if (node_prev)
-				node_prev->next = node_next ;
-			else
-				*lst = node_next;
-		}
-		node = node->next;
+			switch_node_n_node_next(lst, node);
+		else
+			node = node->next;
 	}
 	return (0);
+}
+//	0 -> last pipe (if not = start of list)
+//	1 -> infile
+//	2 -> here doc
+//	3 -> cmd
+//	4 -> outfile
+//	5 -> outfile append
+//	6 -> next pipe (if not = end of list)
+void sort_prompt(t_node **lst)
+{
+	t_node *node = *lst;
+
+
+	if(!lst)
+		return;
+
 }
