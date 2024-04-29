3234600355b353c0310c1e88fa025ecee03fd6a0
lferro <lferro@student.42lausanne.ch>
2024-04-29 20:34:04 +0200

    palloc libft

--- src/utils.c
+++ src/utils.c
@@ -3,24 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Lu-ni <lucas.nicollier@gmail.com>          +#+  +:+       +#+        */
+/*   By: lferro <lferro@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 22:12:43 by Lu-ni             #+#    #+#             */
-/*   Updated: 2024/04/28 22:12:43 by Lu-ni            ###   ########.fr       */
+/*   Updated: 2024/04/29 20:32:47 by lferro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	*palloc(int size, int elem_size)
-{
-	void	*res;
-
-	if (size == 0)
-		exit(1);
-
-	res = malloc(elem_size * (size + 1));
-	if (!res)
-		exit(1);
-	return (res);
-}
