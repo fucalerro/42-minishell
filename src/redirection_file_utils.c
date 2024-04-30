4becb05560590fed96fabdecf8d1992a6896acd6
Lucas Nicollier <lucas.nicollier@gmail.com>
2024-04-30 16:59:39 +0200

    fix herdoc

--- src/redirection_file_utils.c
+++ src/redirection_file_utils.c
@@ -12,23 +12,14 @@
 
 #include "minishell.h"
 
-int	run_redirection_file(t_node *node)
+int run_outfile(t_node *node_tmp, t_node *node)
 {
-	t_node	*node_tmp;
 	int		return_value;
-
-	node_tmp = node;
 	return_value = 0;
-	while (node_tmp && node_tmp->previous && node_tmp->previous->type != T_PIPE)
-		node_tmp = node_tmp->previous;
 	while (node_tmp && node_tmp->type != T_PIPE)
 	{
 		if (node_tmp->type == T_CMD && node_tmp != node)
-			return (0);
-		else if (node_tmp->type == T_INFILE)
-			return_value = exe_infile(node_tmp);
-		else if (node_tmp->type == T_HEREDOC)
-			return_value = exe_heredoc(node_tmp);
+			break;
 		else if (node_tmp->type == T_OUTFILE)
 			return_value = exe_outfile(node_tmp);
 		else if (node_tmp->type == T_OUTFILE_APPEND)
@@ -37,5 +28,32 @@ int	run_redirection_file(t_node *node)
 			return (return_value);
 		node_tmp = node_tmp->next;
 	}
+	return (return_value);
+}
+
+int	run_redirection_file(t_node *node)
+{
+	t_node	*node_tmp;
+	int		return_value;
+	t_node  *node_save;
+
+	node_tmp = node;
+	return_value = 0;
+	while (node_tmp && node_tmp->previous && node_tmp->previous->type != T_PIPE)
+		node_tmp = node_tmp->previous;
+	node_save = node_tmp;
+	while (node_tmp && node_tmp->type != T_PIPE)
+	{
+		if (node_tmp->type == T_CMD && node_tmp != node)
+			return (0);
+		else if (node_tmp->type == T_INFILE)
+			return_value = exe_infile(node_tmp);
+		else if (node_tmp->type == T_HEREDOC)
+			return_value = exe_heredoc(node_tmp);
+		if (return_value)
+			return (return_value);
+		node_tmp = node_tmp->next;
+	}
+	return_value = run_outfile(node_save, node);
 	return (return_value);
 }
